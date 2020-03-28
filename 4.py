6fac9cf9747b809a2d05b584db380b6e91fc95ae
diiasha <62760963+diiasha@users.noreply.github.com>
2020-03-28 11:01:57 +0600

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,9 +2,7 @@ import pygame
 
 flag = False 
 pygame.init() 
-
 screen = pygame.display.set_mode((720, 480))  
-
 back = pygame.Surface((720, 480)) 
 back.fill((162, 250, 248))                
 back = back.convert()      
@@ -15,7 +13,6 @@ screen.blit(back, (0, 0))
 pygame.display.flip()  
 
 while not flag: 
-        
     for event in pygame.event.get():  
         if event.type == pygame.QUIT: 
             flag = True               
